b9da94d887aae6d2fd0316c3934fd9cff0396e26
Bryan Amezcua <34616679+BryanAmezcua@users.noreply.github.com>
2020-02-06 11:06:59 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const apiURL = 'https://api.yelp.com/v3/businesses/search?term=restaurants&locat
 // setting GET request to YELP API
 const request = require('request');
 
+// search term vars
+let final_term = '';
+let final_location = '';
+let final_sort_by = '';
+
 // set up listener, Port 5000
 app.listen(5000, () => {
     console.log("Listening on port 5000");
@@ -34,7 +39,7 @@ function getYelpData (callBack) {
         // term should equal "restaurant", "food" or "beer" -- anything keyword you want to search
         // location can be anywhere
         // limit is a param that specifies the number of results to return.
-        url: 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=Malibu&limit=9',
+        url: 'https://api.yelp.com/v3/businesses/search?' + 'term=' + final_term + '&location=' + final_location + '&sort_by=' + final_sort_by,
         headers: {
             'Authorization': 'Bearer 4FphwD6tGKVKOyAcxCMxlrhlXEYAPHOQmdQ1q36R8hjaReA7L_Na6lGdZQyV3jJkZHqW0ejiDp5t0LgojC5Cj34nPNcKKJIS2hwYmYkUO1bjq0Pi2zOYRDRx-A0yXnYx'
         }
@@ -44,10 +49,16 @@ function getYelpData (callBack) {
 }
 
 app.get('/results', function (request, response) {
-    console.log('BRYAN TESTING -------------------------------------------------------------------');
-    console.log(response.req.query);
+    
+    console.log(response.req.query.term);
+    console.log(response.req.query.location);
+    console.log(response.req.query.sort_by);
+
+    final_term = response.req.query.term;
+    final_location = response.req.query.location;
+    final_sort_by = response.req.query.sort_by;
 
     getYelpData(function(data) {
-        response.send(response.req.query);
+        response.send(data);
     });
 });
